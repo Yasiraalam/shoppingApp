9ecf8ab4763e5f2767c959e4cd539f9a77340cfe
Yasir alam <yasiralam981@gmail.com>
2025-08-11 16:02:02 +0530

    feat: Initialize iOS workspace and add main application structure
    - Created iOS workspace for the shopping app.
    - Added main application component with basic layout.
    - Defined product constants with sample data for the app.
    - Introduced TypeScript interface for product data structure.
    - Created empty files for detail and home screens in the app.

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+//navigation
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+// screens
+import Home from './screen/home'
+import Detail from './screen/detail'
+
+export type RootStackParamList = {
+  Home: undefined;
+  Detail: { product: Product };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          options={{
+            title: 'Trending Products',
+          }}
+          component={Home} />
+        <Stack.Screen
+          name="Detail"
+          options={{
+            title: 'Detail Products',
+          }}
+          component={Detail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
